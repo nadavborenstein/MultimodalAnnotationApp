e99f3213cdbb78fcbc80d591013d59d66c937afd
Nadav Borenstein <nb@di.ku.dk>
2025-09-05 10:18:28 +0200

    Updated GUI

--- app.py
+++ app.py
@@ -6,16 +6,39 @@ from glob import glob
 from st_files_connection import FilesConnection
 import io
 from PIL import Image
+from collections import Counter
 
 conn = st.connection("gcs", type=FilesConnection)
 
 
 NOTES = "annotation-experiment/data/tweets_with_images.csv"
-MAX_ANNOTATIONS_PER_WORKER = 50
+MAX_ANNOTATIONS_PER_WORKER = 10  # TODO: adjust as needed
 ID_COL = "tweetId"
 IMAGE_FOLDER = "annotation-experiment/static/images/"
 PROGRESS_FOLDER = "annotation-experiment/data/worker_progress"
 DONE_FILE = "annotation-experiment/data/done.txt"
+NON_PARTICIPANTS_FILE = "annotation-experiment/data/non_participants.txt"
+NUM_ANNOTATORS_PER_ITEM = 3  # TODO: adjust as needed
+POSITIVE_EMOTIONS = ["hope", "joy", "pride", "curiosity"]
+NEGATIVE_EMOTIONS = ["fear", "anger", "sadness", "ridicule"]
+
+
+def append_to_file(item: str, file_path: str):
+    done = conn.fs.open(file_path, "r").read()
+    done += f"{item}\n"
+    conn.fs.open(file_path, "w").write(done)
+
+
+def record_non_participation():
+    if not st.session_state.worker_id:
+        return
+    if conn.fs.exists(NON_PARTICIPANTS_FILE):
+        append_to_file(st.session_state.worker_id, NON_PARTICIPANTS_FILE)
+    else:
+        conn.fs.open(NON_PARTICIPANTS_FILE, "w").write(
+            f"{st.session_state.worker_id}\n"
+        )
+    st.success("Your choice has been recorded. Thank you.")
 
 
 @st.cache_resource
@@ -23,60 +46,74 @@ def load_notes() -> pd.DataFrame:
     notes = conn.fs.open(NOTES, "r").read()
     notes = pd.read_csv(io.StringIO(notes))
     # seed from worker_id
-    seed = hash(st.session_state.worker_id) % (2**31)
-    notes = notes.sample(frac=1, random_state=seed).reset_index(drop=True)
     images = conn.fs.glob(f"{IMAGE_FOLDER}*.png")
     image_names = [os.path.basename(img) for img in images]
     notes = notes[notes["image_name"].isin(image_names)]
     notes = notes.drop_duplicates(subset=["image_name"])
+    notes.set_index(ID_COL, inplace=True, drop=False)
     return notes
 
 
 def load_done() -> set:
     done = conn.fs.open(DONE_FILE, "r").read()
-    done = set(done.split("\n"))
-    done = {d.strip() for d in done if d}
+    done = done.split("\n")
+    done = [d.strip() for d in done if d]
+    counts = Counter(done)
+    done = {d for d, c in counts.items() if c >= NUM_ANNOTATORS_PER_ITEM}
+
     return done
 
 
-def get_worker_session(worker_id: str):
+def get_worker_session(worker_id: str, notes: pd.DataFrame) -> pd.DataFrame:
     # check if a progress file exists for this worker
     progress_file = f"{PROGRESS_FOLDER}/progress_{worker_id}.csv"
     if conn.fs.exists(progress_file):
         progress = conn.fs.open(progress_file, "r").read()
         progress = pd.read_csv(io.StringIO(progress))
+        progress.set_index(ID_COL, inplace=True, drop=False)
         return progress
     else:
-        notes = load_notes()
+        seed = hash(st.session_state.worker_id) % (2**31)
         done_notes = load_done()
-        notes = notes[~notes[ID_COL].isin(done_notes)]
+        notes = notes[~notes.index.isin(done_notes)]
+
         notes_to_label = notes.sample(
-            n=min(MAX_ANNOTATIONS_PER_WORKER, len(notes)), random_state=42
+            n=min(MAX_ANNOTATIONS_PER_WORKER, len(notes)), random_state=seed
         )
-        ids_to_label = notes_to_label[ID_COL].tolist()
+        ids_to_label = notes_to_label.index.tolist()
         progress = pd.DataFrame(
             {
-                "worker_id": [worker_id] * len(ids_to_label),
                 ID_COL: ids_to_label,
+                "worker_id": [worker_id] * len(ids_to_label),
                 "done": [None] * len(ids_to_label),
                 "label": [None] * len(ids_to_label),
             }
         )
+        progress.set_index(ID_COL, inplace=True, drop=False)
         s = progress.to_csv(index=False)
         conn.fs.open(progress_file, "w").write(s)
         return progress
 
 
+def get_item_number(progress: pd.DataFrame) -> int:
+    done = progress["done"].notnull().sum()
+    return done + 1
+
+
 def select_next_item_for_worker_id(progress: pd.DataFrame) -> str:
+    # select the next item that is not done
     not_done = progress[progress["done"].isnull()]
     if not_done.empty:
         return None
-    next_id = not_done.iloc[0][ID_COL]
+    next_id = not_done.index[0]
     return next_id
 
 
 def clear_selections():
-    for key in ["fear", "anger", "hope", "joy", "none"]:
+    """
+    Clear all selections in the session state.
+    """
+    for key in POSITIVE_EMOTIONS + NEGATIVE_EMOTIONS + ["none"]:
         if key in st.session_state:
             st.session_state[key] = False
     for key in ["other_positive", "other_negative"]:
@@ -87,9 +124,13 @@ def clear_selections():
         st.session_state["emotion_label"] = []
 
 
-def collect_selected_labels():
+def collect_selected_labels() -> list:
+    """
+    Collect selected labels from the session state.
+    Returns a list of selected labels.
+    """
     selected_labels = []
-    for emotion in ["fear", "anger", "hope", "joy", "none"]:
+    for emotion in POSITIVE_EMOTIONS + NEGATIVE_EMOTIONS + ["none"]:
         if st.session_state.get(emotion, False):
             selected_labels.append(emotion)
     other_positive = st.session_state.get("other_positive", "")
@@ -113,22 +154,22 @@ def collect_selected_labels():
 
 
 def confirm_label(progress: pd.DataFrame, note: pd.Series):
+    """
+    Confirm the selected label and update the progress.
+    """
     progress_file = f"{PROGRESS_FOLDER}/progress_{st.session_state.worker_id}.csv"
     selected_labels = collect_selected_labels()
 
     if not selected_labels:
         return
 
-    index = progress[progress[ID_COL] == note[ID_COL]].index
-    if index.empty:
-        st.error("Error: Note ID not found in progress.")
-        return
-    index = index[0]
+    index = note[ID_COL]
     progress.at[index, "done"] = True
     progress.at[index, "label"] = str(selected_labels)
     clear_selections()
     s = progress.to_csv(index=False)
     conn.fs.open(progress_file, "w").write(s)
+    append_to_file(index, DONE_FILE)
 
 
 st.title("Annotation experiment")
@@ -142,6 +183,7 @@ st.radio(
     index=0,
     help="You must consent to participate in this study to proceed.",
     on_change=lambda: st.session_state.update({"show_consent": False}),
+    disabled="worker_id" in st.session_state,
 )
 
 
@@ -158,7 +200,10 @@ elif st.session_state.consent == "No":
         key="worker_id",
         placeholder="ID",
         value=st.session_state.get("worker_id", ""),
+        disabled="worker_id" in st.session_state,
     )
+    if st.session_state.worker_id:
+        record_non_participation()
     st.stop()
 else:
     st.warning("Please provide your consent to proceed.")
@@ -174,6 +219,8 @@ st.text_input(
     key="worker_id",
     placeholder=placeholder,
     value=st.session_state.get("worker_id", ""),
+    disabled="worker_id" in st.session_state,
+    help="Your Prolific ID is used to track your progress and ensure you do not annotate the same item multiple times.",
 )
 if not st.session_state.worker_id:
     st.warning("Please enter your Prolific ID to proceed.")
@@ -183,9 +230,10 @@ else:
         f"Thank you for providing your Prolific ID: {st.session_state.worker_id}."
     )
 
-with st.expander("Instructions", expanded=True):
-    st.markdown(
-        """
+st.header("Instructions")
+expander = st.expander("Instructions", expanded=True, icon="❗️")
+expander.markdown(
+    """
     Please read the following instructions carefully before proceeding with the annotation task.
     
     You will be shown a series of images extracted from tweets. Your task is to annotate the emotions expressed in each image. The possible emotions are categorized into positive and negative emotions.
@@ -196,17 +244,17 @@ with st.expander("Instructions", expanded=True):
     - None of the above: If none of the listed emotions are present, you can select this option.
     
     """
-    )
+)
 
 
 notes = load_notes()
-progress = get_worker_session(st.session_state.worker_id)
+progress = get_worker_session(st.session_state.worker_id, notes=notes)
 next_item_id = select_next_item_for_worker_id(progress)
 if next_item_id is None:
     st.success("You have completed all your annotations. Thank you!")
     st.stop()
 
-note = notes[notes[ID_COL] == next_item_id].iloc[0]
+note = notes.loc[next_item_id]
 image_path = os.path.join(IMAGE_FOLDER, note["image_name"])
 image_data = conn.fs.open(image_path, "rb").read()
 # image = Image.open(io.BytesIO(image_data))
@@ -214,6 +262,8 @@ image_data = conn.fs.open(image_path, "rb").read()
 # height = max(800, image_height)
 # width = int((height / image_height) * image_width)
 
+item_number = get_item_number(progress=progress)
+st.header(f"Annotating item {item_number} out of {len(progress)}")
 st.image(
     image_data,
     caption="Image to annotate",
@@ -223,14 +273,15 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.header("Positive Emotions")
-    st.checkbox("Hope", key="hope")
-    st.checkbox("Joy", key="joy")
+    for emotion in POSITIVE_EMOTIONS:
+        st.checkbox(emotion.capitalize(), key=emotion)
+
     st.text_input("Other positive emotions (comma separated)", key="other_positive")
 
 with col2:
     st.header("Negative Emotions")
-    st.checkbox("Fear", key="fear")
-    st.checkbox("Anger", key="anger")
+    for emotion in NEGATIVE_EMOTIONS:
+        st.checkbox(emotion.capitalize(), key=emotion)
     st.text_input("Other negative emotions (comma separated)", key="other_negative")
 
 st.header("None of the above")
@@ -242,4 +293,5 @@ st.button(
     on_click=lambda: confirm_label(progress=progress, note=note),
     key="confirm_button",
     disabled=not collect_selected_labels(),
+    help="Please select at least one emotion before confirming.",
 )
