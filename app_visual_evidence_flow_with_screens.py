1c63c68eef9ebe816f52d0ecaafa04a564abb394
Nadav Borenstein <nb@di.ku.dk>
2025-10-24 16:35:20 +0200

    visual evidence

--- app_visual_evidence_flow_with_screens.py
+++ app_visual_evidence_flow_with_screens.py
@@ -671,7 +671,7 @@ with container:
             st.markdown(
                 f"<p>"
                 + "<br>" * 10
-                + '</p><div style="background-color:{colour};Height:auto" dir="auto"><h3>{title}</h3>{note_text}</div>',
+                + f'</p><div style="background-color:{colour};Height:auto" dir="auto"><h3>{title}</h3>{note_text}</div>',
                 unsafe_allow_html=True,
             )
 
