757cd96300faf928327471cb11adaf502e78560a
Nadav Borenstein <nb@di.ku.dk>
2025-10-24 16:19:56 +0200

    visual evidence

--- app_visual_evidence_flow_with_screens.py
+++ app_visual_evidence_flow_with_screens.py
@@ -664,7 +664,7 @@ with container:
         # )
         # # st.write(tweet_text)
         # st.markdown("---")
-        with st.container(border=False, vertical_alignment="center"):
+        with st.container(border=False, vertical_alignment="bottom"):
             theme = st_theme()["base"]
             colour = "#E8F6FF" if theme == "light" else "#003554"
             title = "Additional context 💡"
